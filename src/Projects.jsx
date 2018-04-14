438f283844f5482597e2c6a9b4d97bccb66cda85
YehEmily <emilyyeh1997@gmail.com>
2018-04-14 14:11:45 -0400

    ADD: All images for projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import geode from './images/geode-logo.png';
 import gemfire from './images/gemfire.png';
 import poe from './images/marionette.jpg';
@@ -9,6 +10,9 @@ import projects from './images/project-projects.png';
 import lights from './images/lights.png';
 import scanner from './images/scanner.png';
 import robot from './images/robot.png';
+import olin from './images/olin-logo.png';
+import modsim from './images/modsim.jpg';
+import softdes from './images/softdes.png';
 
 class Projects extends Component {
 	render() {
@@ -22,7 +26,6 @@ class Projects extends Component {
 		var highlight4 = { color: 'rgb(107, 213, 255)' };
 
 		return (
-			<div>
 			<div id="resume" className="fill-page">
 			  <div className="margin-top">
 			    <h1>WHAT I DID <span style={highlight1}>THIS SUMMER</span></h1>
@@ -39,7 +42,7 @@ class Projects extends Component {
 			    <div className="cursor-pointer" id="projects"><img src={projects} style={size10} /></div>
 
 			    <br/><br/>
-			    <div className="cursor-pointer" id="puppets" /><img src={poe} style={size10} /></div>
+			    <div className="cursor-pointer" id="puppets"><img src={poe} style={size10} /></div>
 			    <br/><br/>
 
 			    <div className="cursor-pointer" id="labs"><img src={lights} style={size10} /></div> &emsp; 
@@ -48,13 +51,13 @@ class Projects extends Component {
 
 			    <h1>WHAT I DID <span style={highlight3}>LAST SUMMER</span></h1>
 			    <h5>MAY 2016 - AUGUST 2016</h5>
-			    <div className="cursor-pointer" id="bio"><img src="olin-logo.png" style={width10} /></div>
+			    <div className="cursor-pointer" id="bio"><img src={olin} style={width10} /></div>
 			    
 			    <h1>WHAT I DID <span style={highlight4}>AS A FRESHMAN</span></h1>
 			    <h5>SEPTEMBER 2015 - MAY 2016</h5>
 
-			    <div className="cursor-pointer" id="modsim"><img src="modsim.jpg" style={size10} /></div> &emsp; 
-			    <div className="cursor-pointer" id="softdes"><img src="softdes.png" style={size10} /></div>
+			    <div className="cursor-pointer" id="modsim"><img src={modsim} style={size10} /></div> &emsp; 
+			    <div className="cursor-pointer" id="softdes"><img src={softdes} style={size10} /></div>
 			  </div>
 			</div>
 		);
